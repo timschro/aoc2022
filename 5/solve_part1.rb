191968178faa362eb3340eec98508bb543f37a3a
Tim Schröder <tim.schroeder@digitaspixelpark.com>
2022-12-05 13:12:56 +0100

    Day 5. Array operations.

---
+++ 5/solve_part1.rb
@@ -0,0 +1,24 @@
+count = 0
+
+stacks_input, commands = File.read("input.txt").split("\n\n")
+
+stacks_input = stacks_input.split("\n")
+commands = commands.split("\n")
+number_of_stacks_input = stacks_input.pop.scan(/\d+/).map(&:to_i).max
+
+stacks = Array.new(number_of_stacks_input) { [] }
+
+stacks_input.each do |line|
+   number_of_stacks_input.times do |i|
+      stacks[i].push(line[i*4+1]) unless line[i*4+1] == " "
+   end
+end
+
+commands.each do |command| 
+   amount, source, target = command.scan(/\d+/).map(&:to_i)
+   amount.times do
+      stacks[target-1].prepend(stacks[source-1].shift) unless stacks[source-1].nil?
+   end
+end
+
+puts stacks.collect {|stack| stack.first}.join
